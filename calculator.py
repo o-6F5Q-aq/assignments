42dee91287e60ce5f87bdf383491e22afc6c4426
o-6F5Q-aq <stephane.genot@gmail.com>
2018-01-09 00:26:19 -0500

    5*6 using the multiplying function

--- calculator.py
+++ calculator.py
@@ -10,3 +10,8 @@ def substract(a,b)
 
 def divide(a,b)
 	return a/b
+
+# This part of the code uses the multiply function
+print "I'm going to use the calculator functions to multiply 5 and 6"
+x = multiply(5,6)
+print x
